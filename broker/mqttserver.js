301b3ec2795e412432f71b0fd82aae05cdfa2fa6
June <grath90@163.com>
2019-01-25 10:39:40 +0800

    Create mqttserver.js

---
+++ broker/mqttserver.js
@@ -0,0 +1,50 @@
+var mosca = require("mosca");
+
+var ascoltatore = {
+	//using ascoltatore
+	// type: 'mongo',
+	// url: 'mongodb://localhost:27017/mqtt',
+	// pubsubCollection: 'ascoltatori',
+	// mongo: {}
+};
+
+var settings = {
+	port: 1883,
+	backend: ascoltatore,
+	http: {
+		port: 3000,
+		bundle: true,
+		static: './'
+	}
+};
+
+var server = new mosca.Server(settings);
+
+// fired when the mqtt server is ready
+server.on('ready', function() {
+	console.log('Mosca Server is up and running ....');
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+	var topic = packet.topic;
+	var buffer = packet.payload;
+	console.log("topic:" + topic + ", message:", buffer);
+});
+
+server.on('subscribed', function(topic, client) {
+	console.log("subscribed:", topic);
+});
+
+server.on('unsubscribed', function(topic, client) {
+	console.log("unsubcribed:", topic);
+});
+
+server.on('clientConnected', function(client) {
+	var clientId = client.id;
+	console.log('client connected:', clientId);
+});
+
+server.on('clientDisconnected', function(client) {
+	console.log('client disConnected:', client.id);
+});
